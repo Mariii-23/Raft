8adf06d7f8872b74e539dfd2af71757c891e73b0
matildeopbravo <matildeopbravo@gmail.com>
2023-05-04 12:39:35 +0100

    Implemented init for leader and added type alias NodeID

--- raft/node/leader.py
+++ raft/node/leader.py
@@ -1,4 +1,16 @@
-from node import Node
+from node import Node, NodeID
+from typing import Type
+import logging
+
 
 class Leader(Node):
-    pass
+    # Next log entry to be sent to each server
+    _next_index: dict[NodeID, int]
+    # Index of highest log entry known to be replicated on server
+    _match_index: dict[NodeID, int]
+
+    def __init__(self, node_id: NodeID, node_ids: list[NodeID]):
+        super().__init__(node_id, node_ids)
+        self._next_index = dict.fromkeys(node_ids, 0)
+        self._match_index = dict.fromkeys(node_ids, 1)
+        logging.info("Leader %s initialized", node_id)
--- raft/node/node.py
+++ raft/node/node.py
@@ -3,22 +3,27 @@ from abc import ABC
 from typing import Any
 from key_value_store import KeyValueStore
 
+NodeID = str
+
+
 class Entry:
     term: int
-    command: Any # for now
+    command: Any  # for now
+
 
 class Node(ABC):
-    _node_id: str
-    _node_ids: list[str]
+    _node_id: NodeID
+    _node_ids: list[NodeID]
     _store: KeyValueStore
 
+    # Raft vars
     _current_term: int
-    _voted_for: str
+    _voted_for: NodeID | None
     _log: list[Entry]
     _commit_index = 0
     _last_applied = 0
 
-    def __init__(self, node_id: str, node_ids: list[str]) -> None:
+    def __init__(self, node_id: NodeID, node_ids: list[NodeID]) -> None:
         self._node_id = node_id
         self._node_ids = node_ids
         self._store = KeyValueStore()
