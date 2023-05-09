ff33dec87156bccb6d331f6ab35484c4b9da3873
matildeopbravo <matildeopbravo@gmail.com>
2023-05-10 00:48:20 +0100

    Implemented transition_from in leader and follower.

--- raft/node/follower.py
+++ raft/node/follower.py
@@ -1,4 +1,33 @@
-from node import Node
+import logging
+from node import Node, NodeID
+from time import time
+from utils.ms import reply
+from random import uniform
+
 
 class Follower(Node):
-    pass
+    _next_timeout: float
+
+    def __init__(self, node_id: NodeID, node_ids: list[NodeID]) -> None:
+        super().__init__(node_id, node_ids)
+        self.reset_timeout()
+
+    @classmethod
+    def transition_from(cls, node: Node):
+        logging.info(f"Transitioning from {node} to Follower")
+        return super().transition_from(node)
+
+    def reset_timeout(self) -> None:
+        self._next_timeout = time()  # + uniform() TODO
+
+    def handle_append_entries(self, msg) -> Node:
+        self.reset_timeout()
+
+        if msg.body.term < self._current_term:
+            reply(
+                msg,
+                type="append_entries_response",
+                term=self._current_term,
+                success=False,
+            )
+        # TODO
--- raft/node/node.py
+++ raft/node/node.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 from abc import ABC
 from typing import Any
-from key_value_store import KeyValueStore
 from utils.ms import reply
 import logging
+from key_value_store import KeyValueStore
 
 NodeID = str
 
@@ -11,7 +11,6 @@ NodeID = str
 class Entry:
     term: int
     command: Any  # for now
-
 
     def __init__(self, term: int, command: Any) -> None:
         self.term = term
@@ -43,10 +42,20 @@ class Node(ABC):
         self._commit_index = 0
         self._last_applied = 0
 
+    @classmethod
+    def transition_from(cls, node: Node):
+        node._valid_state = False
+        new_state = cls(node._node_id, node._node_ids)
+        new_state._store = node._store
+        new_state._current_term = node._current_term
+        new_state._voted_for = node._voted_for
+        new_state._log = node._log
+        new_state._commit_index = node._commit_index
+        new_state._last_applied = node._last_applied
+        return new_state
 
     def is_from_client(self, msg) -> bool:
         return msg.src not in [self._node_id] + self._node_ids
-
 
     def handle(self, msg) -> Node:
         # If RPC request or response contains term T > currentTerm:
@@ -56,25 +65,25 @@ class Node(ABC):
                 self._valid_state = False
                 self._current_term = msg.body.term
                 self._voted_for = None
-                return Follower(self).handle(msg) # Follower.from_state(self).handle(msg)
+                return Follower.transition_from(self).handle(msg)
 
         match msg.body.type:
-            case 'read' | 'write' | 'cas':
+            case "read" | "write" | "cas":
                 return self.handle_kvs_op(msg)
 
-            case 'append_entries':
+            case "append_entries":
+                # reply false if term < currentTerm (§5.1)
                 return self.handle_append_entries(msg)
 
-            case 'append_entries_response':
+            case "append_entries_response":
                 return self.handle_append_entries_response(msg)
 
             case _:
-                logging.warning('unknown message type %s', msg.body.type)
+                logging.warning("unknown message type %s", msg.body.type)
                 return self
 
-
     def handle_kvs_op(self, msg) -> Node:
-        reply(msg, type='error', code=11, text='only the leader can handle requests')
+        reply(msg, type="error", code=11, text="only the leader can handle requests")
         return self
 
 
