ae79063aaca1ae16e29d07f61a84815fa0131d7a
zer0-5 <guilherme2001fernandes@hotmail.com>
2023-05-11 17:44:44 +0100

    Fix `apply` to increment `_last_applied`

--- raft/node/node.py
+++ raft/node/node.py
@@ -114,6 +114,7 @@ class Node(ABC):
         Apply commited messages that still weren't applied
         """
         for entry in self._log[self._last_applied + 1 - 1 : self._commit_index + 1 - 1]:
+            self._last_applied += 1
             match entry.command.body.type:
                 case "read":
                     self.apply_read(entry.command)
